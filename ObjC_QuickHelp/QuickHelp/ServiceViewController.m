c3bcbd5e40f0d992a32494f97bded7a27a287cf9
mvong <mvong@usc.edu>
2016-12-08 03:49:16 -0800

    Added pin to sVC

--- ObjC_QuickHelp/QuickHelp/ServiceViewController.m
+++ ObjC_QuickHelp/QuickHelp/ServiceViewController.m
@@ -30,7 +30,7 @@
 
 - (void) setUpInterface {
     self.authorLabel.text = [NSString stringWithFormat:@"Author: %@", [self.servicePostsModel getPostAtIndex:self.servicePostsModel.currentPost].user];
-    self.locationLabel.text = [NSString stringWithFormat:@"Location: %@", [self.servicePostsModel getPostAtIndex:self.servicePostsModel.currentPost].address];
+    self.locationLabel.text = [NSString stringWithFormat:@"Location: %@, %@, %@", [self.servicePostsModel getPostAtIndex:self.servicePostsModel.currentPost].address, [self.servicePostsModel getPostAtIndex:self.servicePostsModel.currentPost].city, [self.servicePostsModel getPostAtIndex:self.servicePostsModel.currentPost].zipcode];
     self.serviceTextView.text = [NSString stringWithFormat:@"Description\n%@", [self.servicePostsModel getPostAtIndex:self.servicePostsModel.currentPost].postDescription];
 }
 
@@ -39,20 +39,34 @@
     // Dispose of any resources that can be recreated.
 }
 
-// Helper function that sets up locationManager
+// Helper function that sets up locationManager && identifies nearby services
 - (void) setUpLocationManager {
-    
     CLGeocoder* geoCoder = [[CLGeocoder alloc] init];
     for(int i = 0; i < [self.servicePostsModel getNumPosts]; i++) {
         NSString* address = [self.servicePostsModel getPostAtIndex:i].address;
         NSString* city = [self.servicePostsModel getPostAtIndex:i].city;
         NSString* zipcode = [self.servicePostsModel getPostAtIndex:i].zipcode;
+        NSString* description = [self.servicePostsModel getPostAtIndex:i].postDescription;
         NSString* fullAddress = [NSString stringWithFormat:@"%@, %@, %@", address, city, zipcode];
-        NSLog(@"Address %d: %@", i, fullAddress);
+        [geoCoder geocodeAddressString:fullAddress completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+            if(error) {
+                NSLog(@"%@", error);
+            } else {
+                CLPlacemark* placemark = [placemarks lastObject];
+                XYZMarker* serviceMarker = [[XYZMarker alloc] init];
+                CLLocation* spotLocation = [[CLLocation alloc] initWithLatitude:placemark.location.coordinate.latitude
+                                                                     longitude:placemark.location.coordinate.longitude];
+                serviceMarker.coordinate = spotLocation.coordinate;
+                serviceMarker.title = @"Service!";
+                serviceMarker.subtitle = description;
+                [self.mapView addAnnotation:serviceMarker];
+                MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance
+                (spotLocation.coordinate, 100, 100);
+                [self.mapView setRegion:region animated:YES];
+            }
+        }];
+        
     }
-    
-//    [geoCoder geocodeAddressString:<#(nonnull NSString *)#> completionHandler:<#^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error)completionHandler#>]
-    
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
@@ -60,28 +74,6 @@
     [self.locationManager requestAlwaysAuthorization];
     [self.locationManager startUpdatingLocation];
     self.mapView.showsUserLocation = YES;
-}
-// Sets up the map locator based on user's location
-- (void)locationManager:(CLLocationManager *)manager
-     didUpdateLocations:(NSArray *)locations {
-    CLLocation *newLocation = [locations lastObject];
-    if (self.startPoint == nil) {
-        self.startPoint = newLocation;
-        self.distanceFromStart = 0;
-        
-        XYZMarker *start = [[XYZMarker alloc] init];
-        start.coordinate = newLocation.coordinate;
-        start.title = @"Start Point";
-        start.subtitle = @"This is where we started!";
-        [self.mapView addAnnotation:start];
-        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance
-        (newLocation.coordinate, 100, 100);
-        [self.mapView setRegion:region animated:YES];
-        NSLog(@"Entered");
-    } else {
-        self.distanceFromStart =
-        [newLocation distanceFromLocation:self.startPoint];
-    }
 }
 // LocationManager failed...most likely because user denied permission
 -(void)locationManager:(CLLocationManager *)manager
